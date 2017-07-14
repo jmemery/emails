1231e799a0ead0c3cd3052a800d727b4dd17ed1c
Jase <jase@jases-air.lan>
2017-07-14 13:53:44 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  importance = true;
+  emails1: any = ['bill@micro.com', 'user@email.com']
+  emails = {
+    email: 'bill@microsoft.com',
+    importance: false,
+    subject: 'test',
+    content: 'this is a test'}
 }
