22a9c9a0d0a3dcc56d4cd0eee672b78f3e7153c6
Jase <jase@jases-air.lan>
2017-07-14 14:03:59 -0700

    updated with ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   importance = true;
-  emails1: any = ['bill@micro.com', 'user@email.com']
-  emails = {
+  emails1: any = ['bIll@micro.com']
+  bill = {
     email: 'bill@microsoft.com',
     importance: false,
     subject: 'test',
     content: 'this is a test'}
+  user ={
+    email: 'user@mail.com',
+    importance: false,
+    subject: 'test',
+    content: 'this is also test'
+  }
+  newUser ={
+    email: 'leah@lovelace.com',
+    importance: 'High',
+    subject: 'test',
+    content: 'this is also test'
+  }
+  timmy ={
+    email: 'timmy@mail.com',
+    importance: 'High',
+    subject: 'The new Microsoft Windows XI is about to release',
+    content: "This was a joke everyone uses Mac now"
+  }
 }
